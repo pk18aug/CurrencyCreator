705619320c5993af87cbae4c0d1a230bf3d1bae4
pankaj kataria <pankajkt1808@gmail.com>
2024-03-26 23:14:03 +0530

    Api change

--- src/hooks/useCurrencyInfo.js
+++ src/hooks/useCurrencyInfo.js
@@ -4,7 +4,8 @@ function useCurrencyInfo(currency) {
   const [data, setdata] = useState({});
   useEffect(() => {
     fetch(
-      `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`
+      // `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`
+      `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`
     )
       .then((data) => data.json())
       .then((res) => setdata(res[currency]));
